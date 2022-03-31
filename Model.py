44dc48321e80e4a47fa05999e49982c154d3b851
Niklas Schemmer <niklasschemmer@googlemail.com>
2022-03-31 13:40:02 +0200

    Document model module

--- Model.py
+++ Model.py
@@ -1,21 +1,54 @@
+"""
+Module to create a neural network model
+
+This model only wraps a neural network model, for usage in this project.
+It is outsourced, so that the implementation could easily exchanged by another model if necessary.
+
+Authors: Dominik Brockmann, Niklas Schemmer
+"""
+from numpy import array, iterable
 import tensorflow as tf
 from tensorflow.keras import layers
 
 class DenseModel(tf.keras.Model):
-    def __init__(self, hidden_units, num_actions):
+    """
+    A dense neural network.
+
+    Then network has flat outputs, which can be defined by 'num_actions'.
+    Also the number of hidden_units can be defined by an iterable.
+    """
+    def __init__(self, hidden_units: iterable, num_actions: int):
+        """
+        Initialize the Model.
+
+        Parameter hidden_units: an iterable of integers, which configure the amount of layers and its amount of neurons.
+        Parameter num_actions: the amount of linear output parameters.
+        """
         super(DenseModel, self).__init__()
         self.input_layer = layers.InputLayer()
+        self.flatten_layer = layers.Flatten()
         self.hidden_layers = []
 
+        # Iterate through hidden units to create hidden_layers
         for hidden_unit in hidden_units:
             self.hidden_layers.append(layers.Dense(hidden_unit, activation = 'tanh'))
 
+        # Linear output for regression classifiers
         self.output_layer = layers.Dense(num_actions, activation = 'linear')
 
     @tf.function
-    def call(self, inputs, **kwargs):
+    def call(self, inputs: array, **kwargs) -> array:
+        """
+        Calling the model on input parameters.
+
+        Parameter inputs: array containing input batches that are of same shape as 'num_actions'.
+        """
         x = self.input_layer(inputs)
+        x = self.flatten_layer(x)
+
+        # Iterately call hidden_layers
         for layer in self.hidden_layers:
             x = layer(x)
         output = self.output_layer(x)
+
         return output
